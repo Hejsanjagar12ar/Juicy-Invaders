2a12f7034835b5a4ff9225fff291ae87ff179a26
TedRosling <ted.rosling@elev.ga.lbs.se>
2024-10-14 15:16:08 +0200

    Started Beat
    BeatCounter started working on it

---
+++ Juicy Invaders/Assets/Scripts/BeatCounter.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatCounter : MonoBehaviour
+{
+    public float beat = 120f;
+    public int timeInBeat;
+    public bool thingsCanHappen = false;
+    
+    void FixedUpdate()
+    {
+        timeInBeat += 1;
+        Debug.Log("Counting");
+        if (timeInBeat == 30)
+        {
+            timeInBeat = 0;
+
+            Debug.Log("TIME IS NOW 30");
+
+        }
+    }
+}
